d45f5ccf272fddc19d48afd66643718a71e96415
kkrajid <rajidkk34@gmail.com>
2023-04-27 11:57:20 +0530

    static error3

--- GrocerySite/settings.py
+++ GrocerySite/settings.py
@@ -127,11 +127,10 @@ STATIC_URL = 'static/'
 
 # STATIC_ROOT=os.path.join(BASE_DIR,'static_root')
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / "static",
 ]
-
 
 
 MEDIA_URL = '/images/'
